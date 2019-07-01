27f9e79444cf2669bbe2d77ebd98f659aba71983
Amalia <amaliavillanueva@gmail.com>
2019-07-01 12:57:56 -0300

    add static data set properties to the schema

--- grails-app/services/graphql/GraphqlManagerService.groovy
+++ grails-app/services/graphql/GraphqlManagerService.groovy
@@ -1,14 +1,24 @@
 package graphql
 
+import java.lang.reflect.Method
+
 import grails.gorm.transactions.Transactional
 import grails.util.Holders
 
 import org.grails.datastore.gorm.neo4j.Neo4jMappingContext
+import org.grails.datastore.mapping.config.Property
+import org.grails.datastore.mapping.model.MappingContext
 import org.grails.datastore.mapping.model.PersistentEntity
+import org.grails.datastore.mapping.model.PersistentProperty
 import org.grails.gorm.graphql.GraphQLEntityHelper
 import org.grails.gorm.graphql.GraphQLServiceManager
 import org.grails.gorm.graphql.entity.dsl.GraphQLMapping
+import org.grails.gorm.graphql.entity.dsl.GraphQLPropertyMapping
 import org.grails.gorm.graphql.entity.operations.ListOperation
+import org.grails.gorm.graphql.entity.property.GraphQLDomainProperty
+import org.grails.gorm.graphql.entity.property.impl.CustomGraphQLProperty
+import org.grails.gorm.graphql.entity.property.impl.PersistentGraphQLProperty
+import org.grails.gorm.graphql.entity.property.manager.GraphQLDomainPropertyManager
 import org.grails.gorm.graphql.fetcher.GraphQLDataFetcherType
 import org.grails.gorm.graphql.fetcher.PaginatingGormDataFetcher
 import org.grails.gorm.graphql.fetcher.impl.ClosureDataFetcher
@@ -18,9 +28,11 @@ import org.grails.gorm.graphql.fetcher.interceptor.InterceptingDataFetcher
 import org.grails.gorm.graphql.fetcher.interceptor.InterceptorInvoker
 import org.grails.gorm.graphql.fetcher.interceptor.QueryInterceptorInvoker
 import org.grails.gorm.graphql.fetcher.manager.GraphQLDataFetcherManager
+import org.grails.gorm.graphql.response.errors.GraphQLErrorsResponseHandler
 import org.grails.gorm.graphql.response.pagination.GraphQLPaginationResponseHandler
 import org.grails.gorm.graphql.types.GraphQLPropertyType
 import org.grails.gorm.graphql.types.GraphQLTypeManager
+import org.grails.gorm.graphql.types.output.ShowObjectTypeBuilder
 
 import core.DataSet
 import core.DataSetAttributeDataFetcher
@@ -48,17 +60,33 @@ class GraphqlManagerService {
 
     private GraphQL currentGraphQL
     Neo4jMappingContext neo4jMappingContext
-    GraphQLTypeManager graphQLTypeManager
     GraphQLDataFetcherManager graphQLDataFetcherManager
     GraphQLPaginationResponseHandler graphQLPaginationResponseHandler
     GraphQLServiceManager graphQLServiceManager
 
+    GraphQLDomainPropertyManager graphQLDomainPropertyManager
+    GraphQLErrorsResponseHandler graphQLErrorsResponseHandler
+    GraphQLTypeManager graphQLTypeManager
+
+    private ShowObjectTypeBuilder showObjectTypeBuilder
+
 
     Map<String, GraphQLInputType> listArguments
+
+    private static Method derivedMethod
+    static {
+        try {
+            derivedMethod = Property.getMethod('isDerived', (Class<?>[]) null)
+        }
+        catch (NoSuchMethodException | SecurityException e) {
+        }
+    }
+
 
     GraphQL getGraphQL() {
         if (!currentGraphQL) {
             currentGraphQL = Holders.applicationContext.getBean('graphQL', GraphQL)
+            showObjectTypeBuilder = new ShowObjectTypeBuilder(graphQLDomainPropertyManager, graphQLTypeManager, graphQLErrorsResponseHandler)
         }
 
         return currentGraphQL
@@ -69,15 +97,20 @@ class GraphqlManagerService {
             listArguments = buildListArguments()
         }
 
-        PersistentEntity entity = neo4jMappingContext.getPersistentEntity('core.DataSet')
-        GraphQLMapping mapping = GraphQLEntityHelper.getMapping(entity)
+
+
+        Params params = new Params()
+
+
         InterceptorInvoker queryInterceptorInvoker = new QueryInterceptorInvoker()
 
+        PersistentEntity entity = neo4jMappingContext.getPersistentEntity('core.DataSet')
+        GraphQLMapping mapping = GraphQLEntityHelper.getMapping(entity)
+
+        GraphQLObjectType objectType = buildGraphQLObject(dataModel, entity)
+        
         GraphQLObjectType.Builder queryType = newObject().name('Query')
         List<GraphQLFieldDefinition.Builder> queryFields = []
-
-
-        GraphQLObjectType objectType = buildGraphQLObject(dataModel)
 
 
         ListOperation listOperation = mapping.operations.list
@@ -147,10 +180,28 @@ class GraphqlManagerService {
     }
 
 
-    private buildGraphQLObject(DataModel dataModel) {
+    private buildGraphQLObject(DataModel dataModel, PersistentEntity entity) {
         List<GraphQLFieldDefinition> definitionList = []
 
         // attr estáticos del dataset
+        List<GraphQLDomainProperty> properties = graphQLDomainPropertyManager.builder().alwaysNullable().getProperties(entity)
+
+        for (GraphQLDomainProperty prop : properties) {
+            if (prop.output) {
+                GraphQLFieldDefinition.Builder field = newFieldDefinition()
+                    .name(prop.name)
+                    .deprecate(prop.deprecationReason)
+                    .description(prop.description)
+
+                if (prop.dataFetcher != null) {
+                    field.dataFetcher(prop.dataFetcher)
+                }
+
+                field.type((GraphQLOutputType) prop.getGraphQLType(graphQLTypeManager, GraphQLPropertyType.OUTPUT))
+
+                definitionList.add(field.build())
+            }
+        }
 
         // attr dinámicos
         for (Attribute attribute : dataModel.attributes) {
@@ -163,14 +214,67 @@ class GraphqlManagerService {
             definitionList.add(graphQLFieldDefinition)
         }
 
+        definitionList.add(graphQLErrorsResponseHandler.getFieldDefinition(graphQLTypeManager))
+
+//        if (!entity.isRoot()) {
+//            obj.withInterface(typeManager.createReference(entity.rootEntity, GraphQLPropertyType.OUTPUT))
+//        }
+
+//
+//        objectTypeCache.put(entity, objectType)
+//        objectType
+
+
         return newObject()
             .name(namingConvention(dataModel.name))
             .fields(definitionList)
             .build()
     }
 
+
+    private GraphQLPropertyMapping getPropertyMapping(PersistentProperty property, GraphQLMapping mapping, boolean id = false) {
+        GraphQLPropertyMapping propertyMapping
+        if (mapping.propertyMappings.containsKey(property.name)) {
+            propertyMapping = mapping.propertyMappings.get(property.name)
+        }
+        else {
+            propertyMapping = new GraphQLPropertyMapping()
+        }
+
+        // ver
+        boolean overrideNullable = false
+        if (overrideNullable) {
+            propertyMapping.nullable(true)
+        }
+        else if (id && propertyMapping.nullable == null) {
+            propertyMapping.nullable(false)
+        }
+
+        // ver
+        if (derivedMethod != null) {
+            Property prop = property.mapping.mappedForm
+            if (derivedMethod.invoke(prop, (Object[]) null)) {
+                propertyMapping.input(false)
+            }
+        }
+        propertyMapping
+    }
+
     // ver el tema d los underscore, debería ser algo tipo vul_def --> vulDef
     private String namingConvention(String name) {
         return name
+    }
+
+
+    private class Params {
+
+        PersistentEntity entity
+        GraphQLMapping mapping
+        InterceptorInvoker queryInterceptorInvoker = new QueryInterceptorInvoker()
+
+        Params() {
+            this.entity = neo4jMappingContext.getPersistentEntity('core.DataSet')
+            this.mapping = GraphQLEntityHelper.getMapping(entity)
+        }
     }
 }
